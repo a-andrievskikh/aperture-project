f442a6f34c7350df141eb02433545916c9b908b5
a-andrievskikh <sn0abg0@gmail.com>
2023-03-02 17:01:21 +0500

    delete: @babel/preset-react options

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -54,16 +54,6 @@ module.exports = {
 					},
 				},
 			},
-			{
-				test: /\.jsx$/i,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-					},
-				},
-			},
 		],
 	},
 	resolve: {
