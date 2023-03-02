1cba8182f240b894e77d57c46205de7530d355cc
a-andrievskikh <sn0abg0@gmail.com>
2023-03-02 17:49:53 +0500

    feat: add mode dev

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
+	mode: 'development',
 	output: {
 		clean: true,
 		filename: '[name].js',
