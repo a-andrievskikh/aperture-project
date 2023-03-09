18cbb70a4ea9e60fda1cbaec59cd9a1e2c3eb319
a-andrievskikh <sn0abg0@gmail.com>
2023-03-09 17:42:05 +0500

    feat: add clean:true

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,6 +14,7 @@ module.exports = {
 	context: path.resolve(__dirname, '../src'),
 	entry: ['@babel/polyfill', 'main.js'],
 	output: {
+		clean: true,
 		path: path.resolve(__dirname, '../dist'),
 	},
 	plugins: [
